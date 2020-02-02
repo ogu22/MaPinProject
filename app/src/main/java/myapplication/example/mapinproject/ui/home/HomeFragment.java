c8a650fd9a3e44836387e84fced48d47e75ecfc6
shun-gondo <gondo.syun0711@gmail.com>
2020-02-02 15:10:36 +0900

    マップタップ時のイベントハンドラの実装

--- app/src/main/java/myapplication/example/mapinproject/ui/home/HomeFragment.java
+++ app/src/main/java/myapplication/example/mapinproject/ui/home/HomeFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,7 +19,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import myapplication.example.mapinproject.R;
 
-public class HomeFragment extends Fragment implements OnMapReadyCallback {
+public class HomeFragment extends Fragment implements OnMapClickListener,OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -46,5 +48,11 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        //TODO: マップタップハンドラーの実装
+        Toast.makeText(getActivity(), "tapped, point=" + point, Toast.LENGTH_SHORT).show();
     }
 }

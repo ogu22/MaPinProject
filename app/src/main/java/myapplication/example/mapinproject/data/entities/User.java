e19c2ebc56b6b9c3a2002c5820fa7d95888b46ef
ogu22 <s.oguma22@gmail.com>
2020-02-19 16:50:38 +0900

    data層整理

--- app/src/main/java/myapplication/example/mapinproject/data/entities/User.java
+++ app/src/main/java/myapplication/example/mapinproject/data/entities/User.java
@@ -2,44 +2,29 @@ package myapplication.example.mapinproject.data.entities;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 @IgnoreExtraProperties
-public class User {
+public class User implements Serializable {
     private String userId;
-    private String password;
     private String userName;
-    private String email;
     private String imagePath;
     private String comment;
-    private boolean flg;
 
-    public User() {}
-
-    public User(String userId, String password, String userName, String email, String imagePath, String comment, boolean flg) {
+    public User(String userId, String userName, String imagePath, String comment) {
         this.userId = userId;
-        this.password = password;
         this.userName = userName;
-        this.email = email;
         this.imagePath = imagePath;
         this.comment = comment;
-        this.flg = flg;
     }
 
     public String getUserId() {
         return userId;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public String getUserName() {
         return userName;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public String getImagePath() {
@@ -50,39 +35,29 @@ public class User {
         return comment;
     }
 
-    public boolean isFlg() {
-        return flg;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return userId == user.userId &&
-                flg == user.flg &&
-                password.equals(user.password) &&
-                userName.equals(user.userName) &&
-                email.equals(user.email) &&
-                imagePath.equals(user.imagePath) &&
-                comment.equals(user.comment);
+        return Objects.equals(userId, user.userId) &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(imagePath, user.imagePath) &&
+                Objects.equals(comment, user.comment);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId, password, userName, email, imagePath, comment, flg);
+        return Objects.hash(userId, userName, imagePath, comment);
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "userId=" + userId +
-                ", password='" + password + '\'' +
+                "userId='" + userId + '\'' +
                 ", userName='" + userName + '\'' +
-                ", email='" + email + '\'' +
                 ", imagePath='" + imagePath + '\'' +
                 ", comment='" + comment + '\'' +
-                ", flg=" + flg +
                 '}';
     }
 }
